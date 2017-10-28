f80f3dfda08d816526ce443d404aa7b682be23e0
darkhipo <darkhipo@gmail.com>
2017-10-27 19:21:56 -0700

    Init 3

--- src/test/scala/info/darkhipo/etlChron/HelloSpec.scala
+++ src/test/scala/info/darkhipo/etlChron/HelloSpec.scala
@@ -1,10 +1,9 @@
 package etlChron
 
 import org.scalatest._
-import Matchers._
 
 class HelloSpec extends FlatSpec with Matchers {
-  "The Main object" should startWith "Hello" in {
-    Main.greeting startWith "Hello"
+  "The Main object" should "startWith \"Hello\"" in {
+    Main.greeting should startWith("Hello")
   }
 }
